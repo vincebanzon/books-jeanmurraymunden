98bf2c32047f4bed5d7dd70f009fc09759634b6c
Vince Banzon <vincebanzon@gmail.com>
2019-09-15 10:45:11 +0800

    Update cover photo (homepage) and change navbar to always display. drop CTB lower. change height of cover photo

--- js/agency.js
+++ js/agency.js
@@ -28,11 +28,13 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
+    // if ($("#mainNav").offset().top > 100) {
+    //   $("#mainNav").addClass("navbar-shrink");
+    // } else {
+    //   $("#mainNav").removeClass("navbar-shrink");
+    // }
       $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
+
   };
   // Collapse now if page is not at top
   navbarCollapse();
